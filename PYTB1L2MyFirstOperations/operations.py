131eab94a331754995eb0b97dfea41d2c157887e
leon kruijer <32770@ma-web.nl>
2021-09-23 13:09:39 +0200

    added files

---
+++ PYTB1L2MyFirstOperations/operations.py
@@ -0,0 +1,11 @@
+additions = (50 + 20)
+subtraction = (60 - 35)
+multiply = (6 * 5)
+divide = (700 / 55)
+modulo = (8 % 7)
+
+print("50 + 20 =", additions)
+print("60 - 35 =", subtraction)
+print("6 * 5 =", multiply)
+print("700 / 55 =", divide)
+print("8 % 7 =", modulo)
