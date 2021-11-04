17d7a4bcc05ce217037d38a862ea999890087d12
leon kruijer <32770@ma-web.nl>
2021-11-04 11:47:14 +0100

    added files

---
+++ PYBT1L6CanYouhandelTheList/list.py
@@ -0,0 +1,11 @@
+randomlist = [1.1, 2.2, 4.5, 1, 'apple', 'candy', True, False,]
+
+randomlist.pop(5)
+print(randomlist)
+
+randomlist.append('candy')
+print(randomlist)
+
+x = randomlist.count(1)
+print(x)
+
